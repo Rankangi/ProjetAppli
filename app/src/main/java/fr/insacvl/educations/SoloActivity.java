84019bcaa18fe8cc023ea3a95cd92343d18326c7
erichter <elric.richter@insa-cvl.fr>
2020-06-19 23:07:04 +0200

    added textbox

--- app/src/main/java/fr/insacvl/educations/SoloActivity.java
+++ app/src/main/java/fr/insacvl/educations/SoloActivity.java
@@ -2,12 +2,37 @@ package fr.insacvl.educations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SoloActivity extends AppCompatActivity {
+
+    private EditText textbox;
+
+    private View.OnKeyListener keylistener = new View.OnKeyListener(){
+
+        @Override
+        public boolean onKey(View view, int i, KeyEvent keyEvent) {
+            if((keyEvent.getAction() == KeyEvent.ACTION_DOWN)&&(i==KeyEvent.KEYCODE_ENTER)){
+                TextView text;
+                text = findViewById(R.id.enteredText);
+                text.setText(textbox.getText());
+                return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solo);
+        textbox = findViewById(R.id.getTheWord);
+        textbox.setOnKeyListener(keylistener);
     }
+
+
+
 }

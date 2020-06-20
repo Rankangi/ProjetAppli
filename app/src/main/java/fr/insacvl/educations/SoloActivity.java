8514f6a230846f737a0f54fd8bf31435c0e4612b
erichter <elric.richter@insa-cvl.fr>
2020-06-20 23:18:21 +0200

    text to speech

--- app/src/main/java/fr/insacvl/educations/SoloActivity.java
+++ app/src/main/java/fr/insacvl/educations/SoloActivity.java
@@ -1,5 +1,6 @@
 package fr.insacvl.educations;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -7,9 +8,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class SoloActivity extends AppCompatActivity {
 
     private EditText textbox;
+    private TextToSpeech ttobj;
+
+    
 
     private View.OnKeyListener keylistener = new View.OnKeyListener(){
 
@@ -19,6 +25,7 @@ public class SoloActivity extends AppCompatActivity {
                 TextView text;
                 text = findViewById(R.id.enteredText);
                 text.setText(textbox.getText());
+                ttobj.speak(String.valueOf(textbox.getText()),TextToSpeech.QUEUE_FLUSH,null);
                 return true;
             }
             return false;
@@ -31,6 +38,12 @@ public class SoloActivity extends AppCompatActivity {
         setContentView(R.layout.activity_solo);
         textbox = findViewById(R.id.getTheWord);
         textbox.setOnKeyListener(keylistener);
+        ttobj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                ttobj.setLanguage(Locale.FRENCH);
+            }
+        });
     }
 
 

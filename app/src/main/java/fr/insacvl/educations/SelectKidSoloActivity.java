632a2f070f12eda0717003861c5d6ea2a34f170b
Ducros Valentin <valentin.ducros@insa-cvl.fr>
2020-06-22 15:34:18 +0200

    Ajout jolie liste des enfants dans l'activity solo

--- app/src/main/java/fr/insacvl/educations/SelectKidSoloActivity.java
+++ app/src/main/java/fr/insacvl/educations/SelectKidSoloActivity.java
@@ -1,49 +1,58 @@
 package fr.insacvl.educations;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import fr.insacvl.educations.helper.DatabaseHelper;
 import fr.insacvl.educations.modele.Enfant;
 
-public class SelectKidSoloActivity extends ListActivity {
+public class SelectKidSoloActivity extends Activity {
     DatabaseHelper db;
-    ArrayAdapter<String> adapter;
     List<Enfant> list;
+    ListView listEnfant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_kid_solo);
+        listEnfant = (ListView) findViewById(R.id.listViewEnfant);
 
         db = new DatabaseHelper(getApplicationContext());
         list = db.getAllEnfants();
-        List<String> str = new ArrayList<>();
+        ArrayList<HashMap<String, String>> str = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> map;
+        String nomEnfant;
+        String upperNomEnfant;
         for (Enfant e: list) {
-            str.add(e.getNom());
-            Log.i("DIM", e.getNom());
+            nomEnfant = e.getNom();
+            upperNomEnfant = nomEnfant.substring(0,1).toUpperCase() + nomEnfant.substring(1);
+            map = new HashMap<String, String>();
+            map.put("nom", upperNomEnfant);
+            map.put("id", ""+e.getId());
+            str.add(map);
         }
-        adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, str);
-        setListAdapter(adapter);
+        SimpleAdapter adapter = new SimpleAdapter(this.getBaseContext(), str, R.layout.card_enfant,
+                new String[] {"nom"}, new int[] {R.id.nom_enfant});
+        listEnfant.setAdapter(adapter);
+        listEnfant.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //HashMap<String, String> map = (HashMap<String, String>) listEnfant.getItemAtPosition(position);  // pour récup les données liées au bouton
+                Intent intent = new Intent(SelectKidSoloActivity.this, SoloActivity.class);
+                Enfant selectedFromList = list.get(position);
+                intent.putExtra("child", selectedFromList);
+                startActivity(intent);
+            }
+        });
     }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent intent = new Intent(this, SoloActivity.class);
-        Enfant selectedFromList = list.get(position);
-        intent.putExtra("child", selectedFromList);
-        startActivity(intent);
-    }
-
-
 }

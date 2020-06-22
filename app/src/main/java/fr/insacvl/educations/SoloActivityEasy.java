c5e5a44057233dc0d9d76c125abdde01e13cb919
Richter <Elric.richter@gmail.com>
2020-06-22 18:41:42 +0200

    added easy difficulty

--- app/src/main/java/fr/insacvl/educations/SoloActivityEasy.java
+++ app/src/main/java/fr/insacvl/educations/SoloActivityEasy.java
@@ -4,14 +4,22 @@ import android.content.Intent;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
@@ -24,8 +32,6 @@ import fr.insacvl.educations.modele.Mot;
 public class SoloActivityEasy extends AppCompatActivity {
     // Get the DB:
     DatabaseHelper db;
-    // initialize variable text input by user
-    private EditText textboxUser;
     // initialize variable buton speech
     private Button speechButton;
     // initialize object for text to speech
@@ -36,42 +42,17 @@ public class SoloActivityEasy extends AppCompatActivity {
     boolean wordfoud = true;
     // the Progress Bar
     private ProgressBar progressBar;
+    ListView listChar;
 
+    private TextView enteredText;
+    private HashMap<String, String> map;
+    private ArrayList<HashMap<String, String>> str = new ArrayList<HashMap<String, String>>();
 
     Enfant child;
 
     // Int to becode child score
     // TODO : remplacer par le score de l'enfant dans le constructeur
     int childscore ;
-
-
-    private View.OnKeyListener keylistener = new View.OnKeyListener(){
-
-        @Override
-        public boolean onKey(View view, int i, KeyEvent keyEvent) {
-            if((keyEvent.getAction() == KeyEvent.ACTION_DOWN)&&(i==KeyEvent.KEYCODE_ENTER)){
-                TextView text;
-                text = findViewById(R.id.enteredTextEasy);
-                text.setText(textboxUser.getText());
-                // on check si le mot entré est le bon
-                if( !wordfoud && dbWord.getContenu().toLowerCase().equals(String.valueOf(text.getText()).toLowerCase())){
-                    // si oui il est trouvé (on aura un nouveau mot avec le speech button)
-                    wordfoud = true;
-                    // on ajoute 10 points
-                    scoreUpdate(10);
-                    // on donne la récompense
-                    ttobj.speak("Bravo",TextToSpeech.QUEUE_FLUSH,null);
-                }
-                else {
-                    ttobj.speak("Ce n'est pas la bonne orthographe",TextToSpeech.QUEUE_FLUSH,null);
-                }
-                // clean de la text box
-                textboxUser.setText("");
-                return true;
-            }
-            return false;
-        }
-    };
 
     // function to update the score
     private void scoreUpdate(int addedScore){
@@ -84,6 +65,7 @@ public class SoloActivityEasy extends AppCompatActivity {
     };
 
     private View.OnClickListener clickListener = new View.OnClickListener() {
+        public int wordlenght;
         @Override
         public void onClick(View view) {
             // pour générer le mot random
@@ -96,11 +78,58 @@ public class SoloActivityEasy extends AppCompatActivity {
             }
             // si oui :
             if(wordfoud) {
+                enteredText.setText("");
                 // choisi un mot random
                 int chosenid = new Random().nextInt(listsize); //((max - min) + 1) + min
                 dbWord = dbWordCount.get(chosenid);
                 // le mot n'est pas trouvé
                 wordfoud = false;
+
+                wordlenght = dbWord.getContenu().length();
+                // On va itérer sur les char du mot et leurs ajouter un id
+                int iter = 0;
+                while(iter<wordlenght) {
+                    map = new HashMap<String, String>();
+                    map.put("char", String.valueOf(dbWord.getContenu().charAt(iter)));
+                    map.put("id", "" + iter);
+                    iter++;
+                    str.add(map);
+                }
+                final SimpleAdapter adapter = new SimpleAdapter(view.getContext(), str, R.layout.letter_selector,
+                        new String[] {"char"}, new int[] {R.id.caractere});
+                // on va set l'adapter
+                listChar.setAdapter(adapter);
+                listChar.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        HashMap<String, String> map = (HashMap<String, String>) listChar.getItemAtPosition(position);  // pour récup les données liées au bouton
+                        String tempTxt = String.valueOf(enteredText.getText());
+                        // on ajoute le char cliqué au txt
+                        tempTxt += map.get("char");
+                        enteredText.setText(tempTxt);
+                        // si c'est la bonne taille
+                        if(tempTxt.length()==wordlenght){
+                            // on check si le mot est bon
+                            if(!wordfoud && String.valueOf(enteredText.getText()).equals(dbWord.getContenu())){
+                                // si oui il est trouvé (on aura un nouveau mot avec le speech button)
+                                wordfoud = true;
+                                // on ajoute 10 points
+                                scoreUpdate(10);
+                                // on donne la récompense
+                                ttobj.speak("Bravo",TextToSpeech.QUEUE_FLUSH,null);
+                                str.clear();
+                                listChar.setAdapter(null);
+                            }
+                            // sinon c'est con
+                            else{
+                                ttobj.speak("Grosse merde",TextToSpeech.QUEUE_FLUSH,null);
+                                enteredText.setText("");
+                            }
+
+                        }
+
+                    }
+                });
             }
 
             ttobj.speak(dbWord.getContenu(),TextToSpeech.QUEUE_FLUSH,null);
@@ -126,9 +155,11 @@ public class SoloActivityEasy extends AppCompatActivity {
         // link progressbar
         progressBar = findViewById(R.id.progressBarIDEasy);
 
+        listChar = findViewById(R.id.listViewCaractère);
+
+        enteredText = findViewById(R.id.enteredTextEasy);
+
         // link textboxuser to the textbox and the listener
-        textboxUser = findViewById(R.id.getTheWordEasy);
-        textboxUser.setOnKeyListener(keylistener);
         // link speechButton to the textbox and the listener
         speechButton = findViewById(R.id.speechButtonEasy);
         speechButton.setOnClickListener(clickListener);
